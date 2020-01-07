c8b687830a28c67a0068ef519c5268c92754f2f1
Logan Talbot <logandavidtalbot@gmail.com>
2020-01-07 14:31:50 +0000

    batch labs for cli and portal

---
+++ BatchApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BatchApp
+{
+    /// <summary>
+    /// cmd /c %AZ_BATCH_APP_PACKAGE_WHIZLABSAPP%\\BatchApp.exe
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a batch job");
+        }
+    }
+}
