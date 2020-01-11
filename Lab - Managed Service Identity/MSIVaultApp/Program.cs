af30c23eef2ed211c66947c9c33bfdfaefb60a25
Logan Talbot <logandavidtalbot@gmail.com>
2020-01-11 23:01:47 +0000

    Key Vault MSI Lab code

---
+++ Lab - Managed Service Identity/MSIVaultApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSIVaultApp
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/azure/key-vault/tutorial-net-windows-virtual-machine#create-resources-and-assign-permissions
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1: Get a token from the local (URI) Managed Service Identity endpoint, which in turn fetches it from Azure AD
+            var token = GetToken();
+
+            // Step 2: Fetch the secret value from your key vault
+            System.Console.WriteLine(FetchSecretValueFromKeyVault(token));
+        }
+
+        static string GetToken()
+        {
+            WebRequest request = WebRequest.Create("http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net");
+            request.Headers.Add("Metadata", "true");
+            WebResponse response = request.GetResponse();
+            return ParseWebResponse(response, "access_token");
+        }
+
+        static string FetchSecretValueFromKeyVault(string token)
+        {
+            WebRequest kvRequest = WebRequest.Create("https://whizlabs-vault.vault.azure.net/secrets/dbpassword?api-version=2016-10-01");
+            kvRequest.Headers.Add("Authorization", "Bearer " + token);
+            WebResponse kvResponse = kvRequest.GetResponse();
+            return ParseWebResponse(kvResponse, "value");
+        }
+
+        private static string ParseWebResponse(WebResponse response, string tokenName)
+        {
+            string token = String.Empty;
+            using (Stream stream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                String responseString = reader.ReadToEnd();
+
+                JObject joResponse = JObject.Parse(responseString);
+                JValue ojObject = (JValue)joResponse[tokenName];
+                token = ojObject.Value.ToString();
+            }
+            return token;
+        }
+    }
+}
