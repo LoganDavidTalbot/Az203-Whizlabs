a7f7e54e951f40899484b849462c578fb0295fe7
Logan Talbot <logandavidtalbot@gmail.com>
2020-01-11 23:02:33 +0000

    Key Vault MSI Lab Commands

--- Lab - Managed Service Identity/powershell.ps1
+++ Lab - Managed Service Identity/powershell.ps1
@@ -4,5 +4,8 @@ $response = Invoke-WebRequest -Uri 'http://169.254.169.254/metadata/identity/oau
 $content = $response.Content | ConvertFrom-Json
 # Get ARM Token
 $ArmToken = $content.access_token
-
 echo $ArmToken
+
+
+# 
+az keyvault set-policy --name 'whizlabs-vault' --object-id 7f923227-305b-4d79-912d-ac9e4145cab5 --secret-permissions get list
