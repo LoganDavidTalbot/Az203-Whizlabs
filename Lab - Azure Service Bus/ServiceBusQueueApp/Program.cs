149fc05a83f3fb05f9adfb1f80c005940fc55091
Logan Talbot <logandavidtalbot@gmail.com>
2020-01-17 14:13:55 +0000

    Service Bus Queue Lab

---
+++ Lab - Azure Service Bus/ServiceBusQueueApp/Program.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceBusQueueApp
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://whizlabs-sb1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=+K1h90f6mGWMjBRZOcvM9j8KddP4KD5yhW+DJCJaODg=";
+        const string QueueName = "demo";
+        static IQueueClient queueClient;
+        static void Main(string[] args)
+        {
+            //Write_Queue().GetAwaiter().GetResult();
+            Read_Queue().GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+
+        private static async Task Write_Queue()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            string messageBody = $"Message Text";
+            var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+
+            try
+            {
+                // Write the body of the message to the console.
+                Console.WriteLine($"Sending message: {messageBody}");
+
+                // Send the message to the queue.
+                await queueClient.SendAsync(message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }     
+            await queueClient.CloseAsync();
+        }
+
+        private static async Task Read_Queue()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            RegisterOnMessageHandlerAndReceiveMessages();
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the MessageHandler Options in terms of exception handling, number of concurrent messages to deliver etc.
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of Concurrent calls to the callback `ProcessMessagesAsync`, set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
+                AutoComplete = false
+            };
+
+            // Register the function that will process messages
+            queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            // Process the message
+            Console.WriteLine($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the queueClient is created in ReceiveMode.PeekLock mode (which is default).
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+
+            // Note: Use the cancellationToken passed as necessary to determine if the queueClient has already been closed.
+            // If queueClient has already been Closed, you may chose to not call CompleteAsync() or AbandonAsync() etc. calls 
+            // to avoid unnecessary exceptions.
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity Path: {context.EntityPath}");
+            Console.WriteLine($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+    }
+}
